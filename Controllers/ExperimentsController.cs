5cb4926a473cf1f07a419b1230d935de4f0d47d5
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-14 11:14:19 +0500

    Add project files.

---
+++ Controllers/ExperimentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using Npgsql;
+
+
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExperimentsController : ControllerBase
+    {
+        private string _pgConnector = "Host=localhost;Port=5432;Database=TestDB;username=postgres;Password=Akramjon_09;";
+
+        [HttpGet]
+        public List<experiment> Get(int id)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(_pgConnector))
+            {
+                connection.Open();
+                string query = $"select id,name from experiment where id = {id};";
+                using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+                var result = cmd.ExecuteReader();
+                List<experiment> list = new List<experiment>();
+
+                while (result.Read())
+                {
+                    list.Add(new experiment
+                    {
+                        id = (int)result[0],
+                        name = (string)result[1]
+                    }) ;
+                }
+
+
+                return list;
+            }
+        }
+
+        
+    }
+}
---
+++ experiment.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3
+{
+    public class experiment
+    {
+        public int id {  get; set; }    
+        public string name { get; set; }
+       
+    }
+}
